f3bcc07691ff9104c99382ea5d38d81e63da613f
Ricky513125 <lilingyu513125@163.com>
2025-01-27 19:31:47 +0800

    configuration

--- conf/conf.go
+++ conf/conf.go
@@ -15,39 +15,43 @@ type AppConfig struct {
 }
 
 type App struct {
-	Database Database `yaml:"database"`
-	Redis Redis `yaml:"redis"`
-	FlushAllForTest bool `yaml:"flushAllForTest"`
+	Database        Database `yaml:"database"` // yaml to correspond to yaml
+	Redis           Redis    `yaml:"redis"`
+	FlushAllForTest bool     `yaml:"flushAllForTest"`
 }
 
 type Database struct {
-	Type string `yaml:"type"`
-	User string `yaml:"user"`
+	Type     string `yaml:"type"`
+	User     string `yaml:"user"`
 	Password string `yaml:"password"`
-	DbName string `yaml:"dbName"`
-	Address string `yaml:"address"`
-	MaxIdle int `yaml:"maxIdle"`
-	MaxOpen int `yaml:"maxOpen"`
+	DbName   string `yaml:"dbName"`
+	Address  string `yaml:"address"`
+	MaxIdle  int    `yaml:"maxIdle"`
+	MaxOpen  int    `yaml:"maxOpen"`
 }
 
 type Redis struct {
-	Address string `yaml:"address"`
-	Network string `yaml:"network"`
-	Password string `yaml:"password"`
-	MaxIdle int `yaml:"maxIdle"`
-	MaxActive int `yaml:"maxActive"`
-	IdleTimeout int `yaml:"idleTimeout"`
+	Address     string `yaml:"address"`
+	Network     string `yaml:"network"`
+	Password    string `yaml:"password"`
+	MaxIdle     int    `yaml:"maxIdle"`
+	MaxActive   int    `yaml:"maxActive"`
+	IdleTimeout int    `yaml:"idleTimeout"`
 }
 
-func GetAppConfig() (appConfig AppConfig, err error)  {
+func GetAppConfig() (appConfig AppConfig, err error) {
 	err = yaml.Unmarshal(configFile, &appConfig)
+	// the configFile. to use the configFile's GetAppConfig
+	// use yaml to turn the configFile to an appConfig struct
 	return appConfig, err
 }
 
-func init()  {
+func init() {
 	var err error
+	// ioutil.ReadFile() to turn into configFile
 	configFile, err = ioutil.ReadFile(configFilePath)
 	if err != nil {
 		log.Fatalf("yamlFile. Get err %v", err)
 	}
+
 }
