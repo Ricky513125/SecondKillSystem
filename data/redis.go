f943e23972e3110e640e5d8ad591a35cf674df59
Ricky513125 <lilingyu513125@163.com>
2025-01-30 11:16:40 +0800

    redis

--- data/redis.go
+++ data/redis.go
@@ -9,15 +9,17 @@ var client *redis.Client
 
 // open redis connection pool
 func initRedisConnection(config conf.AppConfig) {
+	// use the default connections to the database
 	client = redis.NewClient(&redis.Options{
 		Addr:     config.App.Redis.Address,
-		Password: config.App.Redis.Password, // It's ok if password is "".
-		DB:       0,                         // use default DB
+		Password: config.App.Redis.Password,
+		DB:       0,
 	})
-
+	// clear all the keys when just connect to Redis
 	if _, err := FlushAll(); err != nil {
 		println("Error when flushAll. " + err.Error())
 	}
+
 }
 
 // use for tests
@@ -28,6 +30,7 @@ func FlushAll() (string, error) {
 // ensure redis to load the lua script, if not then load
 func PrepareScript(script string) string {
 	// sha := sha1.Sum([]byte(script))
+	// check if scripts are available  // []bool
 	scriptsExists, err := client.ScriptExists(script).Result()
 	if err != nil {
 		panic("Failed to check if script exists: " + err.Error())
@@ -43,8 +46,9 @@ func PrepareScript(script string) string {
 	return ""
 }
 
-// 执行lua脚本
+// execute lua script by SHA
 func EvalSHA(sha string, args []string) (interface{}, error) {
+	// Result() *cmd() -> (interface{}, error)
 	val, err := client.EvalSha(sha, args).Result()
 	if err != nil {
 		print("Error executing evalSHA... " + err.Error())
@@ -55,11 +59,16 @@ func EvalSHA(sha string, args []string) (interface{}, error) {
 
 // redis operation SET
 func SetForever(key string, value interface{}) (string, error) {
-	val, err := client.Set(key, value, 0).Result() // expiration表示无过期时间
+	// set a key-value pair in Redis with no expiration time(0).
+	// It turns the result of the SET command
+	val, err := client.Set(key, value, 0).Result() // expiration: no expire time
 	return val, err
 }
 
 // redis operation hmset
+// This function sets multiple fields of a Redis hash.
+// The hash is stored under the given key and contains the key-value pairs specified
+// in the field map.
 func SetMapForever(key string, field map[string]interface{}) (string, error) {
 	return client.HMSet(key, field).Result()
 }
@@ -70,6 +79,8 @@ func GetMap(key string, fields ...string) ([]interface{}, error) {
 }
 
 // redis SADD
+// This function adds a value (field) to a Redis set (key).
+// If the field is already a member of the set, Redis will ignore it.
 func SetAdd(key string, field string) (int64, error) {
 	return client.SAdd(key, field).Result()
 }
