24f42999c624089e160f7e6851bce484f1b1151a
Ricky513125 <lilingyu513125@163.com>
2025-02-03 22:16:33 +0800

    dbService

--- api/dbService/coupon.go
+++ api/dbService/coupon.go
@@ -7,21 +7,22 @@ import (
 )
 
 func GetAllCoupons() ([]model.Coupon, error) {
+	// return all the coupons
 	var coupons []model.Coupon
 	result := data.Db.Find(&coupons)
 	return coupons, result.Error
 }
-//下面原本应该使用gorm封装好的函数来操作数据库的，但是由于当时出了bug，没时间处理，所以直接写数据库命令来操作。
-// 插入用户拥有优惠券的数据
+
+// Insert the data of coupon users have.
 func UserHasCoupon(userName string, coupon model.Coupon) error {
-	return data.Db.Exec(fmt.Sprintf("INSERT IGNORE INTO coupons " +
-		"(`username`,`coupon_name`,`amount`,`left`,`stock`,`description`) " +
+	return data.Db.Exec(fmt.Sprintf("INSERT IGNORE INTO coupons "+
+		"(`username`,`coupon_name`,`amount`,`left`,`stock`,`description`) "+
 		"values('%s', '%s', %d, %d, %f, '%s')",
 		userName, coupon.CouponName, 1, 1, coupon.Stock, coupon.Description)).Error
 }
 
-// 优惠券库存自减1
+// the stock of coupon in the database decrease by 1
 func DecreaseOneCouponLeft(sellerName string, couponName string) error {
-	return data.Db.Exec(fmt.Sprintf("UPDATE coupons c SET c.left=c.left-1 WHERE " +
+	return data.Db.Exec(fmt.Sprintf("UPDATE coupons c SET c.left=c.left-1 WHERE "+
 		"c.username='%s' AND c.coupon_name='%s' AND c.left>0", sellerName, couponName)).Error
 }
--- api/dbService/user.go
+++ api/dbService/user.go
@@ -5,6 +5,7 @@ import (
 	"SecKill/model"
 )
 
+// use userName to get the model.User
 func GetUser(userName string) (model.User, error) {
 	user := model.User{}
 	operation := data.Db.Where("username = ?", userName).First(&user)
